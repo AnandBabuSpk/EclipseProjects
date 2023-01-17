382e538ffb17177a95c82f6449325a9a7d723587
anand <anandbabu.18@outlook.com>
2023-01-17 19:49:28 +0800

    commit freemarker project to github

---
+++ SB-FreeMarker-example/src/main/java/com/freemarker/demo/SbFreeMarkerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.freemarker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFreeMarkerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFreeMarkerExampleApplication.class, args);
+	}
+
+}
---
+++ SB-FreeMarker-example/src/test/java/com/freemarker/demo/SbFreeMarkerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freemarker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbFreeMarkerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
